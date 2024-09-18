a1cfb2f532783315c1526d83872a75fb4192803d
Aman Verma <90442567+iosaman503@users.noreply.github.com>
2024-09-18 21:50:43 +0530

    Update contoller.cc

--- contoller.cc
+++ contoller.cc
@@ -1,8 +1,35 @@
-#include "header.h"
+#ifndef SDN_CONTROLLER_H
+#define SDN_CONTROLLER_H
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/ofswitch13-module.h"
+#include "ns3/netanim-module.h"
+#include "ns3/applications-module.h"
+
+#include <vector>
+#include <map>
 
 namespace ns3 {
 
-NS_LOG_COMPONENT_DEFINE ("SDNController");
+class SDNController : public Application {
+public:
+  static TypeId GetTypeId (void);
+  SDNController ();
+  virtual ~SDNController ();
+
+  void SetupSwitch (Ptr<OFSwitch13Device> swtch);
+  void Learn (Ptr<const Packet> packet, const Address& source);
+
+private:
+  virtual void StartApplication (void);
+  virtual void StopApplication (void);
+
+  std::map<Mac48Address, uint32_t> m_macToPort;
+  std::vector<Ptr<OFSwitch13Device>> m_switches;
+};
 
 NS_OBJECT_ENSURE_REGISTERED (SDNController);
 
@@ -54,3 +81,5 @@ SDNController::StopApplication ()
 }
 
 }
+
+#endif // SDN_CONTROLLER_H
