38086548a6dadefc2c1c0dec8745ecd940ea4d2b
Aman Verma <90442567+iosaman503@users.noreply.github.com>
2024-10-08 15:27:35 +0530

    Update main.cc

--- main.cc
+++ main.cc
@@ -5,7 +5,7 @@
 #include <ns3/internet-module.h>
 #include <ns3/network-module.h>
 #include <ns3/ofswitch13-module.h>
-#include <ns3/aqua-sim-ng-module.h> // AquaSim module for underwater communication
+#include <ns3/aqua-sim-ng-module.h>
 
 using namespace ns3;
 
@@ -28,9 +28,11 @@ int main(int argc, char* argv[])
         OFSwitch13Helper::EnableDatapathLogs();
     }
 
+    // Set default parameters for the OpenFlow switches
     Config::SetDefault("ns3::OFSwitch13Helper::ChannelType", EnumValue(OFSwitch13Helper::DEDICATED_CSMA));
     Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue(1400));
 
+    // Create node containers for servers, switches, controllers, and clients
     NodeContainer serverNodes, switchNodes, controllerNodes, clientNodes;
     serverNodes.Create(2);
     switchNodes.Create(3);
@@ -41,47 +43,61 @@ int main(int argc, char* argv[])
     AquaSimHelper aquaSimHelper;
     aquaSimHelper.SetChannelAttribute("Frequency", DoubleValue(25.0)); // Example frequency setup
 
-    // Set up Csma devices (wired network)
+    // Set up CSMA for wired network connections
     CsmaHelper csmaHelper;
     csmaHelper.SetChannelAttribute("DataRate", DataRateValue(DataRate("100Mbps")));
     
     NetDeviceContainer link, switch0Ports, switch1Ports, switch2Ports, serverDevices, clientDevices;
 
-    // Connect switches with AquaSim and Csma
+    // Connecting switches with CSMA links
     link = csmaHelper.Install(NodeContainer(switchNodes.Get(0), switchNodes.Get(1)));
     switch0Ports.Add(link.Get(0)); switch1Ports.Add(link.Get(1));
+    
     link = csmaHelper.Install(NodeContainer(switchNodes.Get(1), switchNodes.Get(2)));
     switch1Ports.Add(link.Get(0)); switch2Ports.Add(link.Get(1));
 
+    // Connecting servers to the border switch
     link = csmaHelper.Install(NodeContainer(serverNodes.Get(0), switchNodes.Get(0)));
     serverDevices.Add(link.Get(0)); switch0Ports.Add(link.Get(1));
 
-    // Clients connected with AquaSim
+    link = csmaHelper.Install(NodeContainer(serverNodes.Get(1), switchNodes.Get(0)));
+    serverDevices.Add(link.Get(0)); switch0Ports.Add(link.Get(1));
+
+    // Clients connected to the client switch using AquaSim
     for (size_t i = 0; i < clients; i++)
     {
         link = aquaSimHelper.Install(NodeContainer(clientNodes.Get(i), switchNodes.Get(2)));
         clientDevices.Add(link.Get(0)); switch2Ports.Add(link.Get(1));
     }
 
+    // Create the OpenFlow controller
     Ptr<OFSwitch13InternalHelper> ofQosHelper = CreateObject<OFSwitch13InternalHelper>();
     Ptr<QosController> qosCtrl = CreateObject<QosController>();
     ofQosHelper->InstallController(controllerNodes.Get(0), qosCtrl);
 
+    // Install OpenFlow switches
     OFSwitch13DeviceContainer ofSwitchDevices;
     ofSwitchDevices.Add(ofQosHelper->InstallSwitch(switchNodes.Get(0), switch0Ports));
+    ofSwitchDevices.Add(ofQosHelper->InstallSwitch(switchNodes.Get(1), switch1Ports));
+    ofSwitchDevices.Add(ofQosHelper->InstallSwitch(switchNodes.Get(2), switch2Ports));
 
+    // Install Internet stack on nodes
     InternetStackHelper internet;
     internet.Install(serverNodes);
     internet.Install(clientNodes);
 
+    // Enable pcap traces if tracing is enabled
     if (trace)
     {
         ofQosHelper->EnableOpenFlowPcap("openflow"); // OpenFlow traffic
         csmaHelper.EnablePcap("server", serverDevices.Get(0)); // Capture server traffic
         csmaHelper.EnablePcap("client", clientDevices.Get(0));  // Capture client traffic
         csmaHelper.EnablePcap("switch", switch0Ports.Get(0));    // Capture switch traffic
+        csmaHelper.EnablePcap("switch", switch1Ports.Get(0));    // Capture switch traffic
+        csmaHelper.EnablePcap("switch", switch2Ports.Get(0));    // Capture switch traffic
     }
 
+    // Run the simulation
     Simulator::Stop(Seconds(simTime));
     Simulator::Run();
     Simulator::Destroy();
