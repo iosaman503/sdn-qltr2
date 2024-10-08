50fe420cbc0bb1db279c2aa747838d3ee1c51528
Aman Verma <90442567+iosaman503@users.noreply.github.com>
2024-10-08 15:40:56 +0530

    Update controller.h

--- controller.h
+++ controller.h
@@ -1,48 +1,152 @@
-#ifndef CONTROLLER_H
-#define CONTROLLER_H
+/*
+ * Copyright (c) 2016 University of Campinas (Unicamp)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Luciano Jerez Chaves <ljerezchaves@gmail.com>
+ */
+
+#ifndef QOS_CONTROLLER_H
+#define QOS_CONTROLLER_H
 
 #include <ns3/ofswitch13-module.h>
-#include <map>
 
 using namespace ns3;
 
+/**
+ * \brief An border OpenFlow 1.3 controller
+ */
 class QosController : public OFSwitch13Controller
 {
-public:
-    QosController();
-    ~QosController() override;
+  public:
+    QosController();           //!< Default constructor.
+    ~QosController() override; //!< Dummy destructor.
 
+    /** Destructor implementation */
     void DoDispose() override;
 
+    /**
+     * Register this type.
+     * \return The object TypeId.
+     */
     static TypeId GetTypeId();
 
-    // Handle packet-in message
+    /**
+     * Handle a packet in message sent by the switch to this controller.
+     * \note Inherited from OFSwitch13Controller.
+     * \param msg The OpenFlow received message.
+     * \param swtch The remote switch metadata.
+     * \param xid The transaction id from the request message.
+     * \return 0 if everything's ok, otherwise an error number.
+     */
     ofl_err HandlePacketIn(struct ofl_msg_packet_in* msg,
                            Ptr<const RemoteSwitch> swtch,
                            uint32_t xid) override;
 
-protected:
+  protected:
+    // Inherited from OFSwitch13Controller
     void HandshakeSuccessful(Ptr<const RemoteSwitch> swtch) override;
 
-private:
-    void ConfigureSwitch(Ptr<const RemoteSwitch> swtch);
+  private:
+    /**
+     * Configure the border switch.
+     * \param swtch The switch information.
+     */
+    void ConfigureBorderSwitch(Ptr<const RemoteSwitch> swtch);
 
-    // Q-learning related functions
-    void UpdateQTable(Ipv4Address src, Ipv4Address dst, double reward);
-    Ipv4Address SelectNextHop(Ipv4Address src);
+    /**
+     * Configure the aggregation switch.
+     * \param swtch The switch information.
+     */
+    void ConfigureAggregationSwitch(Ptr<const RemoteSwitch> swtch);
 
-    // Trust-related functions
-    void UpdateTrustTable(Ipv4Address node, double trustValue);
-    double GetTrustValue(Ipv4Address node);
+    /**
+     * Handle ARP request messages.
+     * \param msg The packet-in message.
+     * \param swtch The switch information.
+     * \param xid Transaction id.
+     * \return 0 if everything's ok, otherwise an error number.
+     */
+    ofl_err HandleArpPacketIn(struct ofl_msg_packet_in* msg,
+                              Ptr<const RemoteSwitch> swtch,
+                              uint32_t xid);
 
-    // Data structures for Q-learning and trust
-    std::map<std::pair<Ipv4Address, Ipv4Address>, double> m_qTable; // Q-table
-    std::map<Ipv4Address, double> m_trustTable; // Trust table for each node
+    /**
+     * Handle TCP connection request
+     * \param msg The packet-in message.
+     * \param swtch The switch information.
+     * \param xid Transaction id.
+     * \return 0 if everything's ok, otherwise an error number.
+     */
+    ofl_err HandleConnectionRequest(struct ofl_msg_packet_in* msg,
+                                    Ptr<const RemoteSwitch> swtch,
+                                    uint32_t xid);
 
-    // Parameters for Q-learning
-    double m_learningRate;
-    double m_discountFactor;
-    double m_explorationRate;
+    /**
+     * Extract an IPv4 address from packet match.
+     * \param oxm_of The OXM_IF_* IPv4 field.
+     * \param match The ofl_match structure pointer.
+     * \return The IPv4 address.
+     */
+    Ipv4Address ExtractIpv4Address(uint32_t oxm_of, struct ofl_match* match);
+
+    /**
+     * Create an ARP request packet, encapsulated inside of an Ethernet frame.
+     * \param srcMac Source MAC address.
+     * \param srcIp Source IP address.
+     * \param dstIp Destination IP address.
+     * \return The ns3 Ptr<Packet> with the ARP request.
+     */
+    Ptr<Packet> CreateArpRequest(Mac48Address srcMac, Ipv4Address srcIp, Ipv4Address dstIp);
+
+    /**
+     * Create an ARP reply packet, encapsulated inside of an Ethernet frame.
+     * \param srcMac Source MAC address.
+     * \param srcIp Source IP address.
+     * \param dstMac Destination MAC address.
+     * \param dstIp Destination IP address.
+     * \return The ns3 Ptr<Packet> with the ARP reply.
+     */
+    Ptr<Packet> CreateArpReply(Mac48Address srcMac,
+                               Ipv4Address srcIp,
+                               Mac48Address dstMac,
+                               Ipv4Address dstIp);
+
+    /**
+     * Save the pair IP / MAC address in ARP table.
+     * \param ipAddr The IPv4 address.
+     * \param macAddr The MAC address.
+     */
+    void SaveArpEntry(Ipv4Address ipAddr, Mac48Address macAddr);
+
+    /**
+     * Perform an ARP resolution
+     * \param ip The Ipv4Address to search.
+     * \return The MAC address for this ip.
+     */
+    Mac48Address GetArpEntry(Ipv4Address ip);
+
+    Address m_serverIpAddress;  //!< Virtual server IP address
+    uint16_t m_serverTcpPort;   //!< Virtual server TCP port
+    Address m_serverMacAddress; //!< Border switch MAC address
+    bool m_meterEnable;         //!< Enable per-flow metering
+    DataRate m_meterRate;       //!< Per-flow meter rate
+    bool m_linkAggregation;     //!< Enable link aggregation
+
+    /** Map saving <IPv4 address / MAC address> */
+    typedef std::map<Ipv4Address, Mac48Address> IpMacMap_t;
+    IpMacMap_t m_arpTable; //!< ARP resolution table.
 };
 
-#endif /* CONTROLLER_H */
+#endif /* QOS_CONTROLLER_H */
